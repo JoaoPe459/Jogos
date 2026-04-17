fdb8224b7b26bb30bd9d170b29cbaa4a905a6f34
jonas <jonassales510@gmail.com>
2026-04-17 11:10:54 -0300

    Quando um player tocar um portal, ele vai para o local que o portal for definido

--- PacMan/Player.cpp
+++ PacMan/Player.cpp
@@ -4,7 +4,7 @@
 #include "PacMan.h"
 #include "Engine.h"
 #include "LevelMake.h"
-
+#include "Portal.h"
 
 Player::Player() : Entity() {
     type = PLAYER;
@@ -15,6 +15,21 @@ Player::Player() : Entity() {
 
 void Player::OnCollision(Object* obj) {
     Entity::OnCollision(obj);
+
+    if (obj->Type() == PORTAL) {
+        Portal* p = (Portal*)obj;
+
+        if (Engine::game) {
+            LevelMake* lvl = static_cast<LevelMake*>(Engine::game);
+
+            // Chamamos a função que troca o cenário e os portais
+            lvl->SetStage(p->targetBG);
+
+            // Move the player to the desired position when entering the new map
+            // Example: a fixed or calculated position
+            this->MoveTo(lvl->GetSpawnX(p->targetBG), lvl->GetSpawnY(p->targetBG));
+        }
+    }
 
     // 2. Lógica específica do Player (Comida)
     if (obj->Type() == FOOD) {
@@ -51,7 +66,7 @@ void Player::Control() {
 	// Lógica de ataque (Espaço)
     if (window->KeyDown(VK_SPACE)) { // Dispara uma cacetada enquanto tu apertar espaço
 
-        // 1. Determinar a direção do ataque baseada na velocidade atual ou teclas
+        // 1. Determinar a direção do ataque basada na velocidade atual ou teclas
         // Se o player estiver parado, o ataque sai com um pequeno impulso padrão para a direita
         float attackImpulseX = (targetVX != 0) ? targetVX * 1.5f : (targetVY == 0 ? 200.0f : 0);
         float attackImpulseY = (targetVY != 0) ? targetVY * 1.5f : -50.0f; // Um leve pulinho para cima
